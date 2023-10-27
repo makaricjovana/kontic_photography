3a25b90693f8fe4e58cbfbd7b72db5be594f79bf
makaricjovana <makaricjovana23@gmail.com>
2023-10-27 22:24:41 +0200

    Js added

---
+++ app/js/script.js
@@ -0,0 +1,34 @@
+// Selektujte hamburger ikonu za otvaranje side menija
+const hamburger = document.querySelector('.mobile-nav_hamburger');
+
+// Selektujte ikonu za zatvaranje side menija
+const closeMenu = document.querySelector('.side-menu_cross');
+
+// Selektujte side meni i pozadinsku boju
+const sideMenu = document.querySelector('.side-menu');
+const sideMenuBackground = document.querySelector('.side-menu_background');
+
+// Inicijalno sakrijte side meni
+sideMenu.classList.add('none');
+sideMenuBackground.classList.add('none');
+
+// Funkcija za otvaranje side menija
+function openSideMenu() {
+    // Uklonite klasu 'none' sa side menija i pozadinske boje
+    sideMenu.classList.remove('none');
+    sideMenuBackground.classList.remove('none');
+    
+}
+
+// Funkcija za zatvaranje side menija
+function closeSideMenu() {
+    // Dodajte klasu 'none' na side meni i pozadinsku boju
+    sideMenu.classList.add('none');
+    sideMenuBackground.classList.add('none');
+}
+
+// Dodajte event listener za otvaranje side menija
+hamburger.addEventListener('click', openSideMenu);
+
+// Dodajte event listener za zatvaranje side menija
+closeMenu.addEventListener('click', closeSideMenu);
