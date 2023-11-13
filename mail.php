c57e43563ed7951b8e8a71be200b116e1679efaa
makaricjovana <makaricjovana23@gmail.com>
2023-11-13 16:29:16 +0100

    update

--- mail.php
+++ mail.php
@@ -1,36 +1,34 @@
 <? php
-  if (isset($_POST['name']))
-    $name = $_POST['name'];
-  if (isset($_POST['email']))
-    $email = $_POST['email'];
-  if (isset($_POST['message']))
-    $message = $_POST['message'];
-  if (isset($_POST['subject']))
-    $subject = $_POST['subject'];
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  $subject = $_POST['subject'];
+  header('Content-Type: application/json');
   if ($name === '') {
-    echo "Popunite ovo polje.";
-    die();
+    print json_encode(array('message' => 'Name cannot be empty', 'code' => 0));
+    exit();
   }
   if ($email === '') {
-    echo "Ovo polje mora biti popunjeno.";
-    die();
+    print json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
+    exit();
   } else {
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      echo "Nevažeća mail adresa.";
-      die();
+      print json_encode(array('message' => 'Email format invalid.', 'code' => 0));
+      exit();
     }
   }
   if ($subject === '') {
-    echo "Ovo polje mora biti popunjeno.";
-    die();
+    print json_encode(array('message' => 'Subject cannot be empty', 'code' => 0));
+    exit();
   }
   if ($message === '') {
-    echo "Ovo polje mora biti popunjeno.";
-    die();
+    print json_encode(array('message' => 'Message cannot be empty', 'code' => 0));
+    exit();
   }
   $content = "From: $name \nEmail: $email \nMessage: $message";
   $recipient = "photo.jkontic@gmail.com";
   $mailheader = "From: $email \r\n";
   mail($recipient, $subject, $content, $mailheader) or die("Error!");
-  echo "Email sent!";
+  print json_encode(array('message' => 'Email successfully sent!', 'code' => 1));
+  exit();
 ?>
